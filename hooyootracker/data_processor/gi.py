b953c6c31287df7394e08b6089d2f5a70150f722
Scoofszlo <scoofszlo@gmail.com>
2024-11-07 20:41:37 +0800

    Commit data processing modules

---
+++ hooyootracker/data_processor/gi.py
@@ -0,0 +1,48 @@
+from hooyootracker.extractor.gi import (
+    Game8,
+    PocketTactics,
+    RockPaperShotgun,
+    VG247
+)
+from hooyootracker._logger import Logger
+
+logger = Logger.get_logger(level="INFO")  # Set logging level to INFO
+
+
+def get_data():
+    sources = [Game8(), PocketTactics(), RockPaperShotgun(), VG247()]
+
+    unique_list = set()
+    final_list = []
+    total_duplicate_codes = 0
+
+    logger.info(f"Removing potential duplicate code entries from {len(sources)} sources")
+
+    for source in sources:
+        logger.debug(f"Processing source: {source['source_name']}")
+        code_list = source['code_list']
+
+        for entry in code_list:
+            code = entry["code"]
+
+            if code not in unique_list:
+                unique_list.add(code)
+
+                code_info = {
+                    "source_name": source["source_name"],
+                    "source_url": source["source_url"],
+                    "code": code,
+                    "reward_desc": entry["reward_desc"],
+                    "code_link": f"https://genshin.hoyoverse.com/en/gift?code={code}"
+                }
+
+                final_list.append(code_info)
+                logger.debug(f"Added new code: {code} from {source['source_name']}")
+            else:
+                total_duplicate_codes += 1
+                logger.debug(f"Duplicate code found: {code} ({source['source_name']})")
+
+    logger.info(f"Removed {total_duplicate_codes} duplicate codes")
+    logger.info(f"Total list of codes: {len(final_list)}")
+
+    return final_list
