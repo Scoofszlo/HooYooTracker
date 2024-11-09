d1e94a9628f5cea53690da8e4b04607b42c844d2
Scoofszlo <scoofszlo@gmail.com>
2024-11-09 12:33:01 +0800

    Add console logs when running `run.bat`

--- run.bat
+++ run.bat
@@ -1,21 +1,26 @@
 @echo off
 
-:: Creates virtual environment if not exist and install the external
-:: dependencies needed for program to run
+:: Creates virtual environment if not exist
 IF NOT EXIST .venv (
+    echo Creating virtual environment...
     python -m venv .venv
 )
 
 :: Activate the virtual environment
+echo Activating virtual environment...
 call .venv\Scripts\activate.bat
 
-:: Check if poetry is installed, if not, install it
+:: Check if poetry is installed. If not, install it
+echo Checking for Poetry installation...
 poetry --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
+    echo Poetry not found. Installing Poetry...
     pip install poetry
     :: Install dependencies using poetry
+    echo Installing dependencies with Poetry...
     poetry install
 )
 
 :: Run the web application from webapp.py
+echo Running the web application...
 python webapp.py
