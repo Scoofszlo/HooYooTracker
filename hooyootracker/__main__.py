bae368c5f712b594115980734c468773def21e10
Scoofszlo <scoofszlo@gmail.com>
2024-11-15 13:13:09 +0800

    Move the webapp.py inside the webapp module

---
+++ hooyootracker/__main__.py
@@ -0,0 +1,3 @@
+from hooyootracker.webapp import app
+
+app.run()
--- webapp.py
+++
@@ -1,8 +0,0 @@
-from hooyootracker.webapp.home import create_app
-from waitress import serve
-import webbrowser
-
-if __name__ == "__main__":
-    app = create_app()
-    webbrowser.open("http://127.0.0.1:8080")
-    serve(app, host='0.0.0.0', port=8080)
