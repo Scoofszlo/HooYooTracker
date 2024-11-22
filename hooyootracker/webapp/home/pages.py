c182f0be6cf463b7da57916df6f24afd5dfeb91c
Scoofszlo <scoofszlo@gmail.com>
2024-11-22 22:16:26 +0800

    Add refresh button to update data

--- hooyootracker/webapp/home/pages.py
+++ hooyootracker/webapp/home/pages.py
@@ -1,25 +1,39 @@
-from flask import Blueprint, render_template
-from hooyootracker.data_processor import GenshinImpactDP, ZenlessZoneZeroDP
+from flask import Blueprint, render_template, request, redirect, url_for
 from hooyootracker.constants import CONFIG_FILE_PATH
+from hooyootracker.data_processor.presenter import DataPresenter
 
 
 bp = Blueprint("pages", __name__)
 
 
-@bp.route("/")
+@bp.route("/", methods=['GET', 'POST'])
 @bp.route("/gi")
 def home():
-    gi = GenshinImpactDP()
-    sources = gi.get_sources(config_path=CONFIG_FILE_PATH)
-    code_list = gi.get_data(sources=sources)
+    dp = DataPresenter(
+        game="gi",
+        config_path=CONFIG_FILE_PATH
+    )
+    data = dp.get_data()
 
-    return render_template('gi.html', title='Genshin Impact — HooYooTracker', code_list=code_list)
+    if request.method == 'POST':
+        if request.form.get('refresh_data_gi') == 'Refresh data':
+            dp.update_data()
+            return redirect(url_for('pages.home'))
+
+    return render_template('gi.html', title='Genshin Impact — HooYooTracker', data=data)
 
 
-@bp.route("/zzz")
+@bp.route("/zzz", methods=['GET', 'POST'])
 def zzz_page():
-    zzz = ZenlessZoneZeroDP()
-    sources = zzz.get_sources(config_path=CONFIG_FILE_PATH)
-    code_list = zzz.get_data(sources=sources)
+    dp = DataPresenter(
+        game="zzz",
+        config_path=CONFIG_FILE_PATH
+    )
+    data = dp.get_data()
 
-    return render_template('zzz.html', title='ZZZ — HooYooTracker', code_list=code_list)
+    if request.method == 'POST':
+        if request.form.get('refresh_data_zzz') == 'Refresh data':
+            dp.update_data()
+            return redirect(url_for('pages.zzz_page'))
+
+    return render_template('zzz.html', title='ZZZ — HooYooTracker', data=data)
