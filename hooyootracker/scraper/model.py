6a8076d1446d642e64506c04d89c954281730519
Scoofszlo <scoofszlo@gmail.com>
2025-01-04 17:37:25 +0800

    Use dataclass for code entries

--- hooyootracker/scraper/model.py
+++ hooyootracker/scraper/model.py
@@ -1,20 +1,31 @@
 from abc import abstractmethod
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Union
+from dataclasses import dataclass
 from bs4 import Tag
 from hooyootracker.logger import Logger
 
 logger = Logger()
 
 
+@dataclass
+class CodeEntry:
+    code: str
+    reward_details: str
+
+
 class Scraper:
     def __init__(self, source_name: str, source_url: str):
-        self.code_entries_list: Dict[str, str | List] = {
+        self.code_entries_list: Dict[str, Union[str, List[CodeEntry]]] = {
             "source_name": source_name,
             "source_url": source_url,
             "code_list": []
         }
 
-    def get_data(self, source_name: str, source_url: str):
+    def get_data(
+            self,
+            source_name: str,
+            source_url: str
+    ) -> Optional[Dict[str, Union[str, List[CodeEntry]]]]:
         logger.info(f"Getting data from {source_name} ({source_url})")
 
         # Extracts the data from source that has the container containing
@@ -43,13 +54,13 @@ class Scraper:
 
     @abstractmethod
     def _get_source_data(self, source_url: str) -> Optional[List[Tag]]:
-        """Scrapes source data from specified URL source and returns a Tag
-        containing list of codes and reward details"""
+        """Scrapes source data from specified URL source and returns a list of Tags
+        containing codes and reward details"""
         pass
 
-    def _extract_data(self, item: Tag) -> Optional[Dict[str, str]]:
+    def _extract_data(self, item: Tag) -> Optional[CodeEntry]:
         """Extracts the code and reward description from the entry,
-        organizing them into dictionary, thus returning it"""
+        organizing them into a CodeEntry dataclass, thus returning it"""
         logger.debug(f"Extracting data from entry: {item}")
 
         code = self._get_code(item)
@@ -58,21 +69,18 @@ class Scraper:
         if not code or not reward_details:
             return
 
-        entry_details = {
-            "code": code,
-            "reward_details": reward_details
-        }
+        entry_details = CodeEntry(code, reward_details)
 
         logger.debug(f"Extracted info: {entry_details}")
 
         return entry_details
 
     @abstractmethod
-    def _get_code(self, entry: Tag) -> str:
-        """Extracts the code and return it as a string"""
+    def _get_code(self, entry: Tag) -> Optional[str]:
+        """Extracts the code and returns it as a string"""
         pass
 
     @abstractmethod
-    def _get_reward_details(self, entry: Tag) -> str:
-        """Extracts the reward details and return it as a string"""
+    def _get_reward_details(self, entry: Tag) -> Optional[str]:
+        """Extracts the reward details and returns it as a string"""
         pass
