61188b7117a3be5322ada831ea2bc5ec9ed1d77b
Scoofszlo <scoofszlo@gmail.com>
2025-01-04 16:18:43 +0800

    Remove old scraper module

--- hooyootracker/scraper/gi/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pockettactics import PocketTactics
-from .game8 import Game8
-from .rockpapershotgun import RockPaperShotgun
-from .vg247 import VG247
-
-__all__ = ["PocketTactics", "Game8", "RockPaperShotgun", "VG247"]
--- hooyootracker/scraper/zzz/__init__.py
+++
@@ -1,6 +0,0 @@
-from .game8 import Game8
-from .pockettactics import PocketTactics
-from .polygon import Polygon
-from .vg247 import VG247
-
-__all__ = ["Game8", "PocketTactics", "Polygon", "VG247"]
