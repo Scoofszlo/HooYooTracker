5b6263c270ddcd8291bc66ff0e35e4220d0d18f4
Scoofszlo <scoofszlo@gmail.com>
2025-03-18 14:36:08 +0800

    Use uv to create virtual environment instead for run.bat
    This is way much faster

--- run.bat
+++ run.bat
@@ -1,15 +1,5 @@
 @echo off
 
-:: Creates virtual environment if not exist
-IF NOT EXIST .venv (
-    echo Creating virtual environment...
-    python -m venv .venv
-)
-
-:: Activate the virtual environment
-echo Activating virtual environment...
-call .venv\Scripts\activate.bat
-
 :: Check if uv is installed. If not, install it
 echo Checking for uv installation...
 uv --version >nul 2>&1
@@ -20,9 +10,16 @@ IF %ERRORLEVEL% NEQ 0 (
     echo uv is already installed
 )
 
+:: Creates virtual environment using uv
+uv venv --python=3.10 -q
+
+:: Activate the virtual environment
+echo Activating virtual environment...
+call .venv\Scripts\activate.bat
+
 :: Install/update dependencies using uv
 echo Installing/updating dependencies with uv...
-uv pip install -r pyproject.toml
+uv sync --no-dev
 
 :: Run the HooYooTracker package
 echo.
