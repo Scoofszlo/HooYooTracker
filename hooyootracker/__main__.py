bb64e89f646675cdd97f698525b325206d65bba3
Scoofszlo <scoofszlo@gmail.com>
2025-03-25 21:14:33 +0800

    Add Docker run scripts (#33)
    * Create run script for Docker
    
    * Update file name of run scripts
    
    * Update documentation
    
    * Add the URL for accessing app's GUI
    
    * Update .gitignore
    
    * Update Linux run scripts

--- hooyootracker/__main__.py
+++ hooyootracker/__main__.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     This runs the web app and is serving using the Waitress WSGI server.
     """
 
-    logger.info("HooYooTracker has been started")
+    logger.info("HooYooTracker has been started. Open the app via: http://localhost:8080.")
 
     app = create_app()
     webbrowser.open("http://127.0.0.1:8080")
