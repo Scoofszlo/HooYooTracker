5f7bb0b3d19ac1135df0954782fd2301172f24e4
Scoofszlo <scoofszlo@gmail.com>
2025-03-25 21:18:53 +0800

    Update mount path

--- run-windows-docker.bat
+++ run-windows-docker.bat
@@ -11,6 +11,6 @@ docker volume create hooyootracker_data >nul 2>&1
 :: Run the HooYooTracker through Docker
 echo Starting HooYooTracker...
 echo.
-docker run -q -p 8080:8080 -q -v hooyootracker_data:/app hooyootracker
+docker run -q -p 8080:8080 -q -v hooyootracker_data:/app/program_data hooyootracker
 
 pause
