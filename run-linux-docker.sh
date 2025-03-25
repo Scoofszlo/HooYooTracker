bd80cf2d67b50a625ae2dabb35f6937a54bf8e44
Scoofszlo <scoofszlo@gmail.com>
2025-03-25 21:08:19 +0800

    Update Linux run scripts

--- run-linux-docker.sh
+++ run-linux-docker.sh
@@ -3,12 +3,12 @@
 echo "Setting up HooYooTracker..."
 
 # Build the Docker image
-docker build -q -t hooyootracker .
+sudo docker build -t hooyootracker .
 
 # Create a Docker volume
-docker volume create hooyootracker_data
+sudo docker volume create hooyootracker_data
 
 # Run the HooYooTracker package
 echo "Starting HooYooTracker..."
 echo
-docker run -q -p 8080:8080 -q -v hooyootracker_data:/app hooyootracker
+sudo docker run -q -p 8080:8080 -q -v hooyootracker_data:/app hooyootracker
