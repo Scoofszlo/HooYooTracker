d50c91d8aeb6a9c7340fe20cf5a032e8dda6dbc8
Scoofszlo <scoofszlo@gmail.com>
2026-03-07 21:26:47 +0800

    Make `Card` componet accept `className` prop

--- apps/web/src/components/ui/Card.tsx
+++ apps/web/src/components/ui/Card.tsx
@@ -3,13 +3,15 @@ import clsx from "clsx";
 type CardProps = {
   disabled?: boolean;
   children?: React.ReactNode;
+  className?: string;
 };
 
-export function Card({ disabled, children }: CardProps) {
+export function Card({ disabled, children, className }: CardProps) {
   return (
     <div className={clsx(
-      "bg-md-surface border-md-outline-variant border rounded-xl p-4",
-      disabled && "border-md-outline/12 text-md-on-surface/38"
+      "flex flex-col bg-md-surface border-md-outline-variant border rounded-xl p-4",
+      disabled && "border-md-outline/12 text-md-on-surface/38",
+      className
     )}>
       {children}
     </div>
